0cadf22d1dd6dd85a0ba4fd76406853c0efb1ec8
vishnu dutt <vishnu.dutt@imbuegen.com>
2024-12-05 12:16:02 +0530

    food app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <button className='bg-orange hover:bg-hoverOrange '> let we food</button>
+    </>
+  )
+}
+
+export default App
