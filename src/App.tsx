1d69738a0e2b2dda6c635d6245a6fdb3697f32e4
Shubham K <shubhamk@imbuegen.com>
2024-12-11 10:02:06 +0530

    Adding aspect ration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css"
+// import "./App.css"
 import Login from "./auth/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Signup from "./auth/Signup";
@@ -10,7 +10,6 @@ import MainLayout from "./Layout/MainLayout";
 import Profile from "./components/Profile";
 import SearchPage from "./components/SearchPage";
 import RestaurantDetail from "./components/RestaurantDetail";
-
 
 const appRouter = createBrowserRouter([
   {
@@ -33,29 +32,28 @@ const appRouter = createBrowserRouter([
         path: "/restaurant/:id",
         element: <RestaurantDetail />,
       },
-    ]
+    ],
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "/signup",
-    element: <Signup />
+    element: <Signup />,
   },
   {
     path: "/forgot-password",
-    element: <ForgotPassword />
+    element: <ForgotPassword />,
   },
   {
     path: "/reset-password",
-    element: <ResetPassword />
+    element: <ResetPassword />,
   },
   {
     path: "/verify-email",
-    element: <VerifyEmail />
+    element: <VerifyEmail />,
   },
-
 ]);
 
 function App() {
